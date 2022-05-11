980c71741aeae8c1b0fe1c146b664ca9f4d01b38
Douglas Lacerda <douglas.lacerda@bcrservicos.com.br>
2022-05-11 14:23:47 -0300

    renomeado parametros de ticket.create e update

--- libzen/tickets/__init__.py
+++ libzen/tickets/__init__.py
@@ -5,15 +5,15 @@ import json
 _TICKET_VALID_FIELDS = set(['allow_attachments', 'allow_channelback', 'assignee_email', 'assignee_id', 'attribute_value_ids', 'brand_id', 'collaborator_ids', 'collaborators', 'comment', 'created_at', 'custom_fields', 'description', 'due_at', 'email_cc_ids', 'email_ccs', 'external_id', 'follower_ids', 'followers', 'followup_ids', 'forum_topic_id', 'group_id', 'has_incidents', 'id', 'is_public', 'macro_id', 'macro_ids', 'metadata', 'organization_id', 'priority', 'problem_id', 'raw_subject', 'recipient', 'requester', 'requester_id', 'safe_update', 'satisfaction_rating', 'sharing_agreement_ids', 'status', 'subject', 'submitter_id', 'tags', 'ticket_form_id', 'type', 'updated_at', 'updated_stamp', 'url', 'via', 'via_followup_source_id', 'via_id', 'voice_comment'])
 
 
-def create(**ticket_fields) -> int:
-    if not ticket_fields.get('description'):
+def create(**ticket_props) -> int:
+    if not ticket_props.get('description'):
         raise ValueError('422: campo description é obrigatorio')
     
-    invalid_keys = set(ticket_fields.keys()).difference(_TICKET_VALID_FIELDS)
+    invalid_keys = set(ticket_props.keys()).difference(_TICKET_VALID_FIELDS)
     if invalid_keys:
         raise ValueError(f"Chave {invalid_keys.pop()} não é um campo válido para um objeto de ticket.")
 
-    data = json.dumps({ 'ticket': ticket_fields })
+    data = json.dumps({ 'ticket': ticket_props })
     return next(_send('/api/v2/tickets.json', data, result_page_name='ticket', method='post'))['id']
 
 
@@ -27,12 +27,12 @@ def create_many(tickets:'list[dict]') -> str:
 
 
 # TODO: mover essas checagens duplicadas para função aparte
-def update(ticket_id:'Union[str, int]', **ticket_fields) -> dict:
-    invalid_keys = set(ticket_fields.keys()).difference(_TICKET_VALID_FIELDS)
+def update(ticket_id:'Union[str, int]', **ticket_props) -> dict:
+    invalid_keys = set(ticket_props.keys()).difference(_TICKET_VALID_FIELDS)
     if invalid_keys:
         raise ValueError(f"Chave {invalid_keys.pop()} não é um campo válido para um objeto de ticket.")
 
-    data = json.dumps({ 'ticket': ticket_fields })
+    data = json.dumps({ 'ticket': ticket_props })
     return next(_send('/api/v2/tickets/' + str(ticket_id), data, result_page_name='ticket', method='put'))
 
 
