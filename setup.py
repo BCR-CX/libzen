b3319efd2654043250aabd21f15c9bb0c6e5331f
Douglas Lacerda <douglas.lacerda@bcrservicos.com.br>
2022-07-04 12:47:19 +0000

    fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
       name = 'libzen',
       version = '0.2',
-      description = 'Wrapper para endpoints uteils da zendesk',
+      description = 'Wrapper para endpoints úteis da zendesk',
       author = 'BCR',
       packages = ['libzen']
 )
