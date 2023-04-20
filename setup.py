46cde0c709f30cf9c01e8e3a08435a22562e1d8a
Douglas Lacerda <douglas.lacerda@bcrservicos.com.br>
2023-04-20 10:07:52 -0300

    Incrementado versão para 2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='libzen',
-    version='2.4',
+    version='2.5',
     description='Wrapper para endpoints úteis da zendesk',
     author='BCR',
     packages=['libzen'],
